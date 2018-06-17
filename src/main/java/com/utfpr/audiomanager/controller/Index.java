46609a6ac2c5b4c7400eab4525a59e7d326447c3
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-17 17:30:28 -0300

    Refatorado Index.java

--- src/main/java/com/utfpr/audiomanager/controller/Index.java
+++ src/main/java/com/utfpr/audiomanager/controller/Index.java
@@ -1,22 +1,12 @@
 package com.utfpr.audiomanager.controller;
 
 import com.utfpr.audiomanager.util.ETagUtil;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javassist.compiler.JvstCodeGen;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.DatatypeConverter;
 
 /**
  *
@@ -43,28 +33,4 @@ public class Index extends HttpServlet {
                 .getRequestDispatcher("/index.jsp")
                 .forward(request, response);        
     }
-    
-//    private String getETag (HttpServletRequest request) {
-//        String filePath = request.getServletContext().getRealPath("")
-//                    + File.separator + "index.jsp";
-//        Path path = Paths.get(filePath);
-//
-//        File file = new File(filePath);
-//        if (!file.exists()) {
-//            try {
-//                throw new ServletException("File doesn't exists on server.");
-//            } catch (ServletException ex) {
-//                Logger.getLogger(Index.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
-//        
-//        try {
-//            MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
-//            byte[] sha1 = messageDigest.digest(Files.readAllBytes(path));
-//            return DatatypeConverter.printHexBinary(sha1);
-//        } catch (NoSuchAlgorithmException | IOException ex) {
-//            Logger.getLogger(Index.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return "errado" + path.toString();
-//  }
 }
