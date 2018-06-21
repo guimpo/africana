45622f45293315852fa5084f4bf6c5199f2a36e0
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-21 03:26:39 -0300

    Implementado get e get{id}

--- src/main/java/com/utfpr/audiomanager/controller/audio/AudioController.java
+++ src/main/java/com/utfpr/audiomanager/controller/audio/AudioController.java
@@ -3,7 +3,6 @@ package com.utfpr.audiomanager.controller.audio;
 
 import com.google.gson.Gson;
 import com.utfpr.audiomanager.dao.AudioDao;
-import com.utfpr.audiomanager.dao.UsuarioDao;
 import com.utfpr.audiomanager.model.Audio;
 import com.utfpr.audiomanager.model.Usuario;
 import com.utfpr.audiomanager.util.AppUtil;
@@ -11,69 +10,59 @@ import com.utfpr.audiomanager.util.HorarioUtil;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Base64;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-@WebServlet(name = "AudioController", urlPatterns = {"/api/audio2"})
+@WebServlet(name = "AudioController", urlPatterns = {"/api/audio2/*"})
 @MultipartConfig
 public class AudioController extends HttpServlet {
     private static final String UPLOAD_DIR = "uploads";
+    private static final int SEM_ID = 3;
+    private static final int COM_ID = 4;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {    
               
         Usuario user = (Usuario) request.getAttribute("usuario");
-//        Usuario user = new UsuarioDao()
-//                    .getUsuarioByEmail(email);
-//        System.out.println(user.getId());
-//        try {
-//            user.setId(100L);
-//            user.setEmail("nakaima.prk@gmail.com");
-//            user.setSenha("emailemail");
-//        } catch (Exception ex) {
-//            Logger.getLogger(AudioController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        String uri = request.getRequestURI();     
+        List uriList = Arrays.asList(uri.split("/"));
         
-        
-//        String queryTitulo = request.getParameter("titulo");
-        List<Audio> audios = null;
-//        if (queryTitulo == null) {
-        audios = new AudioDao().getAudiosByUsuario(user);
-//        } else {
-//            audios = new AudioDao().getAudiosByUsuarioAndTitulo(user, queryTitulo);
-//        }
-//        String uploadPath = request.getServletContext().getRealPath("")
-//                                                    + File.separator + "uploads";
-        
-//        HttpSession session = request.getSession(false);
-//        session.setAttribute("audios", audios);
-//        session.setAttribute("uploadpath", uploadPath);
-
-//        if(audios.isEmpty() || audios == null) try {
-//            throw new Exception(user.toString());
-//        } catch (Exception ex) {
-//            Logger.getLogger(AudioController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.print(new Gson().toJson(audios));
+        
+        // ["", "api", "audio2"]
+        switch (uriList.size()) {
+            case SEM_ID:
+                List<Audio> audios = new AudioDao().getAudiosByUsuario(user);          
+                out.print(new Gson().toJson(audios));
+                break;
+            case COM_ID:
+//              System.out.println(uriList);
+                Long id = Long.parseLong(uriList.get(COM_ID - 1).toString());
+                Audio audio = new AudioDao().getAudioById(id);
+                if(audio == null) {
+                    response.sendError(response.SC_NOT_FOUND);
+                    break;
+                }
+                out.print(new Gson().toJson(audio));
+                break;
+            default:
+                out.print(new Gson().toJson(new Exception("boring")));
+                response.setStatus(response.SC_BAD_REQUEST);
+        }
     }
 
     @Override
