1d5ed086ca4538da0489367a8cf4ff52d0062467
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-25 07:14:32 -0300

    Refatorado AudioController

--- src/main/java/com/utfpr/audiomanager/controller/audio/AudioController.java
+++ src/main/java/com/utfpr/audiomanager/controller/audio/AudioController.java
@@ -2,6 +2,7 @@
 package com.utfpr.audiomanager.controller.audio;
 
 import com.google.gson.Gson;
+import com.sun.org.apache.xalan.internal.xsltc.trax.OutputSettings;
 import com.utfpr.audiomanager.dao.AudioDao;
 import com.utfpr.audiomanager.model.Audio;
 import com.utfpr.audiomanager.model.Usuario;
@@ -9,12 +10,21 @@ import com.utfpr.audiomanager.util.AppUtil;
 import com.utfpr.audiomanager.util.HorarioUtil;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -38,39 +48,68 @@ public class AudioController extends HttpServlet {
             throws ServletException, IOException {    
               
         Usuario user = (Usuario) request.getAttribute("usuario");
-        String uri = request.getRequestURI();     
-        List uriList = Arrays.asList(uri.split("/"));
+        
+        String uri = request.getRequestURI();
+        
+        String regex = "/api/audio2/\\d+";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(uri);
+        boolean download = m.matches();
         
         response.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = response.getWriter();
         
-        // ["", "api", "audio2"]
-        switch (uriList.size()) {
-            case SEM_ID:
-                List<Audio> audios = new AudioDao().getAudiosByUsuario(user);          
-                out.print(new Gson().toJson(audios));
-                break;
-            case COM_ID:
-//              System.out.println(uriList);
-                Long id = Long.parseLong(uriList.get(COM_ID - 1).toString());
-                Audio audio = new AudioDao().getAudioById(id);
+        try(ServletOutputStream os = response.getOutputStream()) {
+            if(download) {
+                long id = Long.parseLong(Arrays
+                        .asList(uri.split("/"))
+                        .get(uri.split("/").length - 1));
+
+                Audio audio = new AudioDao().getAudioByUsuarioAndId(user, id);
+                String fileName = audio.getCaminho();
+
                 if(audio == null) {
                     response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                    break;
+                    return;
                 }
-                out.print(new Gson().toJson(audio));
-                break;
-            default:
-                out.print(new Gson().toJson(new Exception("boring")));
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        }
+                
+                String uploadPath = request.getServletContext().getRealPath("")
+                    + File.separator + "uploads";
+                System.out.println(uploadPath + File.separator + fileName);
+                File file = new File(uploadPath + File.separator + fileName);
+                if (!file.exists()) {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                    throw new ServletException("File doesn't exists on server.");
+                }
+                ServletContext ctx = getServletContext();
+                InputStream fis = new FileInputStream(file);
+                String mimeType = ctx.getMimeType(file.getAbsolutePath());
+                response.setContentType(mimeType != null ? mimeType : "application/octet-stream");
+                response.setContentLength((int) file.length());
+                response.setHeader("Content-Disposition", "attachment; filename=\"" + audio.getTitulo() + "\"");
+
+                byte[] bufferData = new byte[1024];
+                int read = 0;
+                while ((read = fis.read(bufferData)) != -1) {
+                    os.write(bufferData, 0, read);
+                }
+                os.flush();
+                fis.close();
+                
+            } else {
+                List<Audio> audios = new AudioDao().getAudiosByUsuario(user);
+                os.print(new Gson().toJson(audios));
+            }              
+        } catch(ServletException se) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }        
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        Usuario currentUser = (Usuario) request.getAttribute("usuario");
+        Usuario currentUser =  (Usuario) request.getAttribute("usuario");
+        
         FileItemFactory factory = new DiskFileItemFactory();
         ServletFileUpload upload = new ServletFileUpload(factory);
         String UPLOAD_DIRECTORY = "uploads";
@@ -107,18 +146,14 @@ public class AudioController extends HttpServlet {
             audio.setCaminho(newFileName);
             audio.setUsuario(currentUser);
             new AudioDao().salvar(audio);
-            response.setContentType("application/json;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.print(new Gson().toJson("yohoho"));
-
-        } catch (FileUploadException e) {
-            response.setContentType("application/json;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.print(new Gson().toJson(e));
-        } catch (Exception e) {
-            response.setContentType("application/json;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.print(new Gson().toJson(e));
+            response.setStatus(HttpServletResponse.SC_CREATED);
+            
+        } catch (FileUploadException e ) {
+            Logger.getLogger(AudioController.class.getName()).log(Level.SEVERE, null, e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } catch (Exception ex) {
+            Logger.getLogger(AudioController.class.getName()).log(Level.SEVERE, null, ex);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -126,15 +161,19 @@ public class AudioController extends HttpServlet {
     protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        String uri = request.getRequestURI();     
-        List uriList = Arrays.asList(uri.split("/"));
+        Usuario user =  (Usuario) request.getAttribute("usuario");
         
+        String uri = request.getRequestURI();     
+                
         response.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+        ServletOutputStream out = response.getOutputStream();
         
         //System.out.println(uriList);
-        Long id = Long.parseLong(uriList.get(COM_ID - 1).toString());
-        Audio audio = new AudioDao().getAudioById(id);
+        long id = Long.parseLong(Arrays
+                    .asList(uri.split("/"))
+                    .get(uri.split("/").length - 1));
+        
+        Audio audio = new AudioDao().getAudioByUsuarioAndId(user, id);
         if(audio == null) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
@@ -168,6 +207,4 @@ public class AudioController extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_NO_CONTENT);
         }
     }
-    
-    
 }
