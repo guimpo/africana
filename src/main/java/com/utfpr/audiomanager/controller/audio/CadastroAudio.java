fca29d5f08af7c3e1c9eb654e06dcfdc507f2c32
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-25 07:14:00 -0300

    Refatorado CadastroAudio

--- src/main/java/com/utfpr/audiomanager/controller/audio/CadastroAudio.java
+++ src/main/java/com/utfpr/audiomanager/controller/audio/CadastroAudio.java
@@ -1,14 +1,7 @@
 package com.utfpr.audiomanager.controller.audio;
 
-import com.utfpr.audiomanager.dao.AudioDao;
-import com.utfpr.audiomanager.model.Audio;
 import com.utfpr.audiomanager.model.Usuario;
-import com.utfpr.audiomanager.util.AppUtil;
-import com.utfpr.audiomanager.util.HorarioUtil;
-import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -16,11 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  *
@@ -38,6 +26,7 @@ public class CadastroAudio extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         Usuario user = (Usuario) session.getAttribute("user");
+        System.out.println("Entrou no get cadastroAudio");
         getServletContext()
                 .getRequestDispatcher("/WEB-INF/view/audio/cadastro.jsp")
                 .forward(request, response);        
@@ -46,52 +35,52 @@ public class CadastroAudio extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        Usuario currentUser = (Usuario) session.getAttribute("user");
-        FileItemFactory factory = new DiskFileItemFactory();
-        ServletFileUpload upload = new ServletFileUpload(factory);
-        String UPLOAD_DIRECTORY = "uploads";
-        String uploadPath = request.getServletContext().getRealPath("")
-            + File.separator + UPLOAD_DIRECTORY;
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdir();
-        }
-        try {
-            List<FileItem> fields = upload.parseRequest(request);
-            System.out.println(fields);
-            Iterator<FileItem> it = fields.iterator();
-            if (!it.hasNext()) {
-                throw new Exception("Erro ao salvar o arquivo.");
-            }
-            String titulo = null;
-            String newFileName = null;
-            while (it.hasNext()) {
-                FileItem fileItem = it.next();
-                boolean isFormField = fileItem.isFormField();
-                if (!isFormField) {
-                    String fileName = new File(fileItem.getName()).getName();
-                    newFileName = AppUtil.encodeString(currentUser.getEmail()) + "_" + HorarioUtil.getCurrentDateString("yyyyMMddHHmmss") + ".mp3";
-                    String filePath = uploadPath + File.separator + newFileName;
-                    File storeFile = new File(filePath);
-                    fileItem.write(storeFile);
-                } else {
-                    titulo = fileItem.getString();
-                }
-            }
-            Audio audio = new Audio();
-            audio.setTitulo(titulo);
-            audio.setCaminho(newFileName);
-            audio.setUsuario(currentUser);
-            new AudioDao().salvar(audio);
-            session.setAttribute("suMessage", "Audio adicionado com sucesso!");
-            response.sendRedirect("lista");
-        } catch (FileUploadException e) {
-            session.setAttribute("erMessage", "Nao foi possivel executar o upload do arquivo.");
-            response.sendRedirect("lista");
-        } catch (Exception e) {
-            session.setAttribute("erMessage", "Nao foi possivel salvar o audio.");
-            response.sendRedirect("lista");
-        }
+        // HttpSession session = request.getSession();
+        // Usuario currentUser = (Usuario) session.getAttribute("user");
+        // FileItemFactory factory = new DiskFileItemFactory();
+        // ServletFileUpload upload = new ServletFileUpload(factory);
+        // String UPLOAD_DIRECTORY = "uploads";
+        // String uploadPath = request.getServletContext().getRealPath("")
+        //     + File.separator + UPLOAD_DIRECTORY;
+        // File uploadDir = new File(uploadPath);
+        // if (!uploadDir.exists()) {
+        //     uploadDir.mkdir();
+        // }
+        // try {
+        //     List<FileItem> fields = upload.parseRequest(request);
+        //     System.out.println(fields);
+        //     Iterator<FileItem> it = fields.iterator();
+        //     if (!it.hasNext()) {
+        //         throw new Exception("Erro ao salvar o arquivo.");
+        //     }
+        //     String titulo = null;
+        //     String newFileName = null;
+        //     while (it.hasNext()) {
+        //         FileItem fileItem = it.next();
+        //         boolean isFormField = fileItem.isFormField();
+        //         if (!isFormField) {
+        //             String fileName = new File(fileItem.getName()).getName();
+        //             newFileName = AppUtil.encodeString(currentUser.getEmail()) + "_" + HorarioUtil.getCurrentDateString("yyyyMMddHHmmss") + ".mp3";
+        //             String filePath = uploadPath + File.separator + newFileName;
+        //             File storeFile = new File(filePath);
+        //             fileItem.write(storeFile);
+        //         } else {
+        //             titulo = fileItem.getString();
+        //         }
+        //     }
+        //     Audio audio = new Audio();
+        //     audio.setTitulo(titulo);
+        //     audio.setCaminho(newFileName);
+        //     audio.setUsuario(currentUser);
+        //     new AudioDao().salvar(audio);
+        //     session.setAttribute("suMessage", "Audio adicionado com sucesso!");
+        //     response.sendRedirect("lista");
+        // } catch (FileUploadException e) {
+        //     session.setAttribute("erMessage", "Nao foi possivel executar o upload do arquivo.");
+        //     response.sendRedirect("lista");
+        // } catch (Exception e) {
+        //     session.setAttribute("erMessage", "Nao foi possivel salvar o audio.");
+        //     response.sendRedirect("lista");
+        // }
     }
 }
