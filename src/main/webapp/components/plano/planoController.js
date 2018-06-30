1028516959e200e3268b1810835a74d013e7f59c
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-30 20:32:30 -0300

    removida diretiva ng-click

--- src/main/webapp/components/plano/planoController.js
+++ src/main/webapp/components/plano/planoController.js
@@ -13,19 +13,4 @@ app.controller('planoController', function ($scope, $http, $location, $route, $l
         $log.info(errResponse);
         $log.error("erro");
       });
-    
-    self.editar = function(id) {
-      $http({
-        method: 'GET',
-        url: $location.protocol() + '://'+ $location.host() +':'+  $location.port()  + '/africana/plano/lista',
-//          params: {email: self.usuario.email, senha: self.usuario.senha}
-    }).then(function (response) {
-      $log.info(response.data);
-      self.planoLista = response.data;
-
-    }, function (errResponse) {
-        $log.info(errResponse);
-        $log.error("erro");
-    });
-    }
 });
