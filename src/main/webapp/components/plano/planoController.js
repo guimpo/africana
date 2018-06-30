1466b839e2e07916845d8139139fc79016f9963b
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-30 19:14:59 -0300

    Removido filtro, corrigido erro de digitação

--- src/main/webapp/components/plano/planoController.js
+++ src/main/webapp/components/plano/planoController.js
@@ -3,18 +3,16 @@ app.controller('planoController', function ($scope, $http, $location, $route, $l
     
     self.planoLista = [];
     
-    $http({
-        method: 'GET',
-        url: $location.protocol() + '://'+ $location.host() +':'+  $location.port()  + '/africana/plano/lista',
-//          params: {email: self.usuario.email, senha: self.usuario.senha}
-    }).then(function (response) {
-      $log.info(response.data);
-      self.planoLista = response.data;
-
-    }, function (errResponse) {
+    var url = $location.protocol() + '://'+ $location.host() +':'+  $location.port()  + '/api/plano';
+    
+    $http.get(url)
+      .then(function (response) {
+        $log.info(response.data);
+        self.planoLista = response.data;
+      }, function (errResponse) {
         $log.info(errResponse);
         $log.error("erro");
-    });
+      });
     
     self.editar = function(id) {
       $http({
