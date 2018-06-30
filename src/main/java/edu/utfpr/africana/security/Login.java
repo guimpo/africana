23d6f27cd26f7a9670bf6d1debf748bcae519c7e
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-30 17:23:53 -0300

    Implementando autenticação básica

--- src/main/java/edu/utfpr/africana/security/Login.java
+++ src/main/java/edu/utfpr/africana/security/Login.java
@@ -3,90 +3,54 @@ package edu.utfpr.africana.security;
 import com.google.gson.Gson;
 import edu.utfpr.africana.dao.UsuarioDao;
 import edu.utfpr.africana.model.Usuario;
-import edu.utfpr.africana.util.ETagUtil;
 import edu.utfpr.africana.util.HashingUtil;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Base64;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "Login", urlPatterns = {"/usuario/entrar"})
+@WebServlet(name = "Login", urlPatterns = {"/login"})
 public class Login extends HttpServlet {
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        HttpSession session = request.getSession();
-        session.invalidate();
-        
-        String eTagFromBrowser = request.getHeader("If-None-Match");
-        String eTagFromServer = ETagUtil.get(request, "/WEB-INF/view/usuario/login.jsp");
-
-        if (eTagFromServer.equals(eTagFromBrowser)) {
-            // retornar código 304
-            response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
-            return;
-        }
-
-        response.addHeader("ETag", ETagUtil.get(request, "/WEB-INF/view/usuario/login.jsp"));
-        
-        response.setContentType("text/html;charset=UTF-8");
-        getServletContext()
-                .getRequestDispatcher("/WEB-INF/view/usuario/login.jsp")
-                .forward(request, response);
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        HttpSession session = request.getSession();
+        response.setContentType("application/json;charset=UTF-8");
         
-        // TODO: adicionar no filtro !
-        response.setContentType("text/plain;charset=UTF-8");
+        BufferedReader reader = request.getReader();
+        Usuario userReceived = new Gson().fromJson(reader, Usuario.class);
         
-        // BufferedReader reader = request.getReader();
-        // Usuario user = new Gson().fromJson(reader, Usuario.class);
-        
-        // String email = user.getEmail();
-        // String senha = user.getSenha();
+        System.out.println(userReceived);
+                        
+        Usuario userStored = new UsuarioDao().getUsuarioByEmail(userReceived.getEmail());
 
-        // System.out.println(user);
-
-        String email = request.getParameter("email");
-        String senha = request.getParameter("senha");
+        if (userStored == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println(new Gson().toJson(new Exception("Usuário ou senha inválido.")));
+        }
         
-        try {
-            
-            Usuario resultUsuario = new UsuarioDao().getUsuarioByEmail(email);
-            
-            if (resultUsuario == null) {
-                throw new Exception("email ou senha inválidos");
-            }
-            
-            boolean isSenhaValid = HashingUtil.validateHashedPassword(senha, resultUsuario.getSenha());
-            
-            if (isSenhaValid) {
-                session.setMaxInactiveInterval(5*60);
-                session.setAttribute("user", resultUsuario);
-                getServletContext()
-                    .getRequestDispatcher("/WEB-INF/view/audio/lista.jsp")
-                    .forward(request, response);
-            } else {
-                throw new Exception("email ou senha inválidos");
-            }
-//        }
-//        catch(IllegalStateException ex) {
-//            response.sendRedirect("../audio/lista");
-        } catch(Exception e) {
-            session.setAttribute("erMessage", e.getMessage());
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        System.out.println((char)27 + "[33m" + "passou do if usuario null");
+        
+        boolean isValidPw = 
+            HashingUtil.validateHashedPassword(userReceived.getSenha(),
+            userStored.getSenha());
+
+        if (isValidPw) {
+            String tokenString = userReceived.getEmail() + ":" + userReceived.getSenha();
+            tokenString = Base64.getEncoder().encodeToString(tokenString.getBytes());
+            Cookie c = new Cookie("credentias", tokenString);
+            c.setMaxAge(-1);
+            c.setPath("/");
+            response.addCookie(c);            
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println(new Gson().toJson(new Exception("Usuário ou senha inválido.")));
         }
     }
 }
