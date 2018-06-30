d31f11dfce96223851821d428db7d85325cb4e32
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-30 19:22:49 -0300

    Adicionado get lista plano

--- src/main/webapp/components/login/loginController.js
+++ src/main/webapp/components/login/loginController.js
@@ -1,10 +1,11 @@
-app.controller('loginController', function ($scope, $http, $location, $route, $log) {
+app.controller('loginController', function ($scope, $http, $location, $log) {
     var self = $scope;
     self.login = function () {
-      var url = $location.protocol() + '://'+ $location.host() +':'+  $location.port()  + '/login';
+      var url = $location.protocol() + '://'+ $location.host() +':'+  $location.port()  + '/api/login';
       $http.post(url, self.usuario)
         .then(function(resp) {
           $log.info(resp);
+          $location.path('/plano/lista');
         }, function(err) {
           $log.error(err);
         });
